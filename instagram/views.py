9831edf29a25a67a42d7dc365ceed327fa009ba6
Ngina07 <phillis.njoroge@strathmore.edu>
2019-11-17 16:12:23 +0300

    add login to view function and display login button on navbar

--- instagram/views.py
+++ instagram/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Post
 
 
 def welcome(request):
     return render(request, 'index.html')
+
+@login_required(login_url='/accounts/register/')
+def index(request):
+    # current_user = request.user
+    post = Posts.get_posts()
+
+    # follow other users
+    return render(request, 'index.html',{"post":post})
