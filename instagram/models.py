fc86dfc6e2fd1c072ab0e7b950e152faed30603d
Ngina07 <phillis.njoroge@strathmore.edu>
2019-11-17 16:28:54 +0300

    link landing page view function

--- instagram/models.py
+++ instagram/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Q
 import datetime as dt
 
 class Post(models.Model):
--- instagram/urls.py
+++ instagram/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns=[
-    url('^$',views.welcome, name='welcome')
+    url('^$',views.welcome, name='welcome'),
+    url(r'^$', views.index,name='index'),
 ]
