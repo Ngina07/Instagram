b13caf847390ba65dfecfe7ef2faa5728517c46e
Ngina07 <phillis.njoroge@strathmore.edu>
2019-11-17 16:02:05 +0300

    add posts model to store posts to database

--- clone/settings.py
+++ clone/settings.py
@@ -77,10 +77,13 @@ WSGI_APPLICATION = 'clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'phillis',
+        'PASSWORD':'123456',
     }
 }
+
 
 
 # Password validation
@@ -107,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
 
-# Create your models here.
+class Post(models.Model):
+    profile_pic = models.ImageField(upload_to = 'profilepics/')
+    caption = models.CharField(max_length=3000)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ImageField(upload_to='posts/')
+    likes = models.IntegerField()
+
+    
+    post_date=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
