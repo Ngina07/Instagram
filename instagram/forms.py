f99999c9e5afaf062b51cf3e7641f4c2b5e70dcb
Ngina07 <phillis.njoroge@strathmore.edu>
2019-11-18 11:53:00 +0300

    add form to upload profile pic

--- instagram/forms.py
+++ instagram/forms.py
@@ -16,3 +16,8 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('profile_pic','website', 'bio', 'location')
+
+class ProfilePicForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('profile_pic',)
