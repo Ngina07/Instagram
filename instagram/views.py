fe97e33436fa39da52def0149ec37a15a93ce7a3
Ngina07 <phillis.njoroge@strathmore.edu>
2019-11-18 10:02:11 +0300

    add method to display user profile

--- instagram/views.py
+++ instagram/views.py
@@ -4,17 +4,19 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from .models import Post, Follow, Comments , Profile
+from .forms import PostForm, UserForm, ProfileForm
 import datetime as dt
+
 
 
 
 @login_required(login_url='/accounts/register/')
 def index(request):
-    # current_user = request.user
+    current_user = request.user
     post = Post.get_posts()
 
     # follow other users
-    return render(request, 'index.html',{"post":post})
+    return render(request, 'index.html',{"post":post,"user":current_user})
 
 @login_required(login_url='/accounts/login/')
 def homepage(request):
@@ -28,8 +30,10 @@ def profile(request,username):
     try:
         user = User.objects.get(username=username)
         profile_pic = Profile.objects.filter(user_id=user).all().order_by('-id')
-        post = Posts.objects.filter(user_id=user).all().order_by('-id')
+        post = Post.objects.filter(username_id=user).all().order_by('-id')
     except ObjectDoesNotExist:
         raise Http404()
 
     return render(request, 'profiles/profile.html', {"post":post, "user":user, "profile_pic":profile_pic})
+
+
